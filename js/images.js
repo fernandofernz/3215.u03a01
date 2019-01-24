6712a47112d76abc16e6ae29aa1b2a79f5bf0dad
Ferny <Fernz.@fernzs-mbp.attlocal.net>
2019-01-24 12:11:30 -0800

    Initial commit

---
+++ js/images.js
@@ -0,0 +1,14 @@
+window.onload = preloadFun;
+
+var arrayIndexCount = 0;
+var imageBanner = new Array ("/images/banner1.jpg", "/images/banner2.jpg", "/images/banner3.jpg");
+
+function preloadFun () {
+    console.log(Array);
+    arrayIndexCount++;
+    if (imageBanner == imageBanner.length) {
+        arrayIndexCount = 0;
+    };
+    document.getElementById("bannerImages").src = imageBanner[arrayIndexCount];
+    setTimeout(preloadFun, 3 * 1000);
+}
