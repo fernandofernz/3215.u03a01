1f16c6e01074d8e7f6e31b498ea82ad06667162a
Ferny <Fernz.@fernzs-mbp.attlocal.net>
2019-01-25 23:11:04 -0800

    var changes

--- js/images.js
+++ js/images.js
@@ -1,7 +1,7 @@
 window.onload = preloadFun;
 
-var arrayIndexCount = 0;
-var imageBanner = new Array ("images/banner1.jpg", "images/banner2.jpg", "images/banner3.jpg");
+let arrayIndexCount = 0;
+let imageBanner = new Array ("images/banner1.jpg", "images/banner2.jpg", "images/banner3.jpg");
 
 function preloadFun () {
     console.log(Array);
