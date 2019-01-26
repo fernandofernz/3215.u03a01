75ed9cb500d1c611f86db44730b51b7689e3823b
Ferny <Fernz.@fernzs-mbp.attlocal.net>
2019-01-25 23:30:38 -0800

    js changes

--- js/images.js
+++ js/images.js
@@ -1,14 +1,19 @@
-window.onload = preloadFun;
+//The JavaScript onload event handler 
+window.onload = preloaderImages;
 
-let arrayIndexCount = 0;
-let imageBanner = new Array ("images/banner1.jpg", "images/banner2.jpg", "images/banner3.jpg");
+//declare  variable 
+var arrayIndex = 0;
+var bannerImages = new Array("images/banner1.jpg", "images/banner2.jpg", "images/banner3.jpg");
 
-function preloadFun () {
-    console.log(Array);
-    arrayIndexCount++;
-    if (imageBanner == imageBanner.length) {
-        arrayIndexCount = 0;
-    };
-    document.getElementById("ban").src = imageBanner[arrayIndexCount];
-    setTimeout(preloadFun, 3 * 1000);
+//The function to rotate the images.
+function preloaderImages() {
+    
+//Hello - What do you think happened here?
+    arrayIndex++;
+    if (arrayIndex == bannerImages.length) {
+        arrayIndex = 0;
+    }
+    document.getElementById("ban").src = bannerImages[arrayIndex];
+
+    setTimeout(preloaderImages, 3 * 1000);
 }
